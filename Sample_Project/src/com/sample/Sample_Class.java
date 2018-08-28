74b28aa7225dc9c17edbb3867187dcec95db738f
unknown <info.praveen@gmail.com>
2018-08-28 14:56:49 +0530

    First Commit

---
+++ Sample_Project/src/com/sample/Sample_Class.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class Sample_Class 
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Hi This is Praven, I am Using newdevice");
+		
+		System.out.println(System.getProperty("user.dir"));
+	}
+
+}
